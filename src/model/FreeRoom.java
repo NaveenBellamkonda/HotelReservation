154562d84eb3b3d0a76e74f280ffcbbad8c48ba2
NaveenBellamkonda <b.naveen1819@gmail.com>
2022-04-28 11:44:28 +0530

    HR

---
+++ src/model/FreeRoom.java
@@ -0,0 +1,5 @@
+package model;
+
+public class FreeRoom extends Room{
+    
+}
---
+++ src/model/Room.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Room implements IRoom{
+    String roomNumber;
+    Double price;
+    RoomType roomType;
+    Boolean free;
+
+    public Room(String roomNumber,Double price,RoomType roomType){
+        this.roomNumber=roomNumber;
+        this.price=price;
+        this.roomType=roomType
+    }
+
+    public String getRoomNumber(){
+        return roomNumber;
+    }
+    public String setRoomNumber(){
+        this.roomNumber=roomNumber;
+    }
+    public Double getRoomPrice(){
+        return price
+    }
+    public RoomType getRoomType(){
+        return roomType;
+    }
+    public Boolean isFree() {
+        return free;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber='" + roomNumber + '\'' +
+                ", price=" + price +
+                ", roomType=" + roomType +
+                ", free=" + free +
+                '}';
+    }
+}
