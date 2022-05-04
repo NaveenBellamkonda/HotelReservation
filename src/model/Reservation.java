5db571eebf71f60c7974c694cef9ede41d237146
NaveenBellamkonda <b.naveen1819@gmail.com>
2022-05-04 16:40:43 +0530

    Cs

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -7,6 +7,12 @@ public class Reservation {
     public IRoom room;
     public Date checkInDate;
     public Date checkOutDate;
+     public Reservation(Customer customer, IRoom room, Date CheckInDate, Date CheckOutDate){
+         this.customer=customer;
+         this.room=room;
+         this.checkInDate=checkInDate;
+         this.checkOutDate=checkOutDate;
+     }
 
     @Override
     public String toString() {
--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -4,9 +4,46 @@ import model.Customer;
 import model.IRoom;
 import model.Reservation;
 
-import java.util.Collection;
-import java.util.Date;
+import java.util.*;
 
 public class ReservationService {
 
+    private Map<String ,IRoom> rooms=new HashMap<String, IRoom>();
+    private Map<String,Collection<Reservation>> reservations=new HashMap<String, Collection<Reservation>>();
+
+    public void addRoom(IRoom room){
+        rooms.put(room.getRoomNumber(),room);
+    }
+    public IRoom getARoom(String RoomID){
+        return rooms.get(RoomID);
+    }
+
+    public Reservation reserveARoom(Customer customer,IRoom room,Date CheckInDate,Date CheckOutDate){
+        Reservation reservation=new Reservation(customer,room,CheckInDate,CheckOutDate);
+        Collection<Reservation> customerReservations=getCustomersReservation(customer);
+        if (customerReservations==null){
+            customerReservations=new LinkedList<Reservation>();
+        }
+        customerReservations.add(reservation);
+        reservations.put(customer.getEmail(), customerReservations);
+        return reservation;
+
+    }
+    public Collection<IRoom> findRooms(Date CheckInDate, Date CheckOutDates){
+        return findAvailableRooms(CheckInDate,CheckOutDates);
+    }
+
+    public Collection<Reservation> getCustomersReservation(Customer customer){
+
+    }
+    public void printAllReservation(){
+        private Collection<Reservation> allReservations=new LinkedList<Reservation>();
+        for(Collection<Reservation> reservations:reservations.values()){
+            allReservations.addAll(reservations);
+        }
+        for(Reservation reservation:allReservations){
+            System.out.println(reservation);
+        }
+
+    }
 }
