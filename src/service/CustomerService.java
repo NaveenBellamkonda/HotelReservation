dc4cc773ae99f7cbfe1351ec2dcefe2a21fd6b5c
NaveenBellamkonda <b.naveen1819@gmail.com>
2022-05-04 15:33:39 +0530

    Cs

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,5 +1,27 @@
 package service;
 
+import model.Customer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CustomerService {
+
+    private Map<String , Customer> customers=new HashMap<String, Customer>();
+
+
+    public void addCustomer(String email, String firstName, String lastName){
+        customers.put(email, new Customer(firstName,lastName,email));
+    }
+
+    public Customer getCustomer(String customerEmail) {
+        return customers.get(customerEmail);
+    }
+
+    public Collection<Customer> getAllCustomers() {
+        return customers.values();
+    }
 
 }
--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -1,4 +1,12 @@
 package service;
 
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+
+import java.util.Collection;
+import java.util.Date;
+
 public class ReservationService {
+
 }
